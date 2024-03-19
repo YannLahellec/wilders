0761676ac7cc28ae45470ef5909c62c22965fa43
YannLahellec <lahellec.yann@gmail.com>
2024-03-19 11:22:53 +0100

    name colors

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+const studentsNames = ['Coline', 'Ryadh', 'Charlie', 'Curtis'];
+
+console.log(chalk.blue(studentsNames[0]));
+console.log(chalk.red(studentsNames[1]));
+console.log(chalk.magenta(studentsNames[2]));
+console.log(chalk.greenBright(studentsNames[3]));
